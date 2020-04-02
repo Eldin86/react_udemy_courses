d24887aa60e8390b0011a7615d038c678795d76d
Edy <maslesaeldin@gmail.com>
2020-04-02 21:54:57 +0200

    finished routing project section 12

--- Section 12 Adding Routing to our Burger Project/section_twelve/src/components/Order/Order.js
+++ Section 12 Adding Routing to our Burger Project/section_twelve/src/components/Order/Order.js
@@ -3,10 +3,28 @@ import React from 'react'
 import classes from './Order.module.css'
 
 const order = (props) => {
-    return(
+    //Pretvaranje objekta u niz, drugi nacin, prvi je u Burger.js
+    const ingredients = []
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        })
+    }
+    console.log('[Order.js -> converted Object]', ingredients)
+    const ingredientOutput = ingredients.map(ing => {
+        return <span style={{
+            textTransform: 'capitalize',
+            display: 'inline-block',
+            margin: '0 8px',
+            border: '1px solid #ccc',
+            padding: '5px'
+        }} key={ing.name}>{ing.name} {ing.amount}</span>
+    })
+    return (
         <div className={classes.Order}>
-            <p>Ingredients: Salad (1)</p>
-    <p>Price: <strong>USD {Number(props.price).toFixed(2)}</strong></p>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>USD {Number(props.price).toFixed(2)}</strong></p>
         </div>
     )
 }
