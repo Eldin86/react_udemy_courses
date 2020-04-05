9c4846b6616693b349c4338e7d85f6654337fe2a
Edy <maslesaeldin@gmail.com>
2020-04-06 01:50:32 +0200

    finished redux introduction section

---
+++ Section 14 Redux/section_fourteen/src/store/reducers/counter.js
@@ -0,0 +1,36 @@
+//Ovo je file koji cemo exportovati reducer koji koristimo
+//actionTypes je objekat posto smo ih sve exportovali, named export, tako da dobijemo objekat
+import * as actionTypes from '../actions'
+//REDUCER
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case actionTypes.INCREMENT:
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case actionTypes.DECREMENT:
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        case actionTypes.ADD:
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
+        case actionTypes.SUBTRACT:
+            return {
+                ...state,
+                counter: state.counter - action.value
+            }
+        default: ;
+    }
+    return state
+}
+
+export default reducer
